f026a2a14b21a8c42ec833cca2edcfc8d1799bd2
jaksmid <bjunior@seznam.cz>
2015-03-03 17:42:10 +0100

    Added authenticate

---
+++ OpenML/Response/Authenticate.cs
@@ -0,0 +1,13 @@
+﻿using RestSharp.Deserializers;
+
+namespace OpenML.Response
+{
+    public class Authenticate
+    {
+
+        [DeserializeAs(Name = "session_hash")]
+        public string Hash { get; set; }
+        
+        public string Timezone { get; set; }
+    }
+}
