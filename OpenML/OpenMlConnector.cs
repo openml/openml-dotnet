07b6f7cf34f2ebcb74efb77103010265f741085a
jaksmid <bjunior@seznam.cz>
2015-03-04 21:19:42 +0100

    Connect method

--- OpenML/OpenMlConnector.cs
+++ OpenML/OpenMlConnector.cs
@@ -9,21 +9,27 @@ namespace OpenML
         private string _user = "";
         private string _password = "";
         private string _urlEndpoint = "http://openml.org/api/";
+        private RestClient _client;
 
         public OpenMlConnector()
         {
-            var client = new RestClient(_urlEndpoint);
-            var request = new RestRequest("?f=openml.authenticate", Method.POST);
-            request.AddParameter("username", _user); 
-            request.AddParameter("password", Utilities.CalculateMd5Hash(_password));
-            IRestResponse<Authenticate> response = client.Execute<Authenticate>(request);
+            _client = new RestClient(_urlEndpoint);
 
+            var authenticate = Connect(_user, _password);
             var request2=new RestRequest("?f=openml.data", Method.POST);
-            request2.AddParameter("session_hash", response.Data.Hash);
-            IRestResponse<Data> response2 = client.Execute<Data>(request2);
+            request2.AddParameter("session_hash", authenticate .Hash);
+            IRestResponse<Data> response2 = _client.Execute<Data>(request2);
             var content2 = response2.Content;
         }
 
+        public Authenticate Connect(string user, string password)
+        {
+            var request = new RestRequest("?f=openml.authenticate", Method.POST);
+            request.AddParameter("username", user);
+            request.AddParameter("password", Utilities.CalculateMd5Hash(password));
+            IRestResponse<Authenticate> response = _client.Execute<Authenticate>(request);
+            return response.Data;
+        }
 
         
     }
