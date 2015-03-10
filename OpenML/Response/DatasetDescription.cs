2cef8327c20107400c6502e56b4a19db7245dba8
jaksmid <bjunior@seznam.cz>
2015-03-10 20:10:09 +0100

    Added dataset description

---
+++ OpenML/Response/DatasetDescription.cs
@@ -0,0 +1,34 @@
+﻿namespace OpenML.Response
+{
+    public class DatasetDescription
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Version { get; set; }
+
+        public string Description { get; set; }
+
+        public string Format { get; set; }
+
+        public string UploadDate { get; set; }
+
+        public string Licence { get; set; }
+
+        public string Url { get; set; }
+        public string Md5CheckSum { get; set; }
+
+        public string VersionLabel { get; set; }
+
+        public string Tag { get; set; }
+
+        public string Visibility { get; set; }
+
+        public string DefaultTargetAttribute { get; set; }
+
+        public string Status { get; set; }
+
+        public string OriginalDataUrl { get; set; }
+    }
+}
