6432cc71a099d41d7a2c67b7a382b070ff57aefa
jaksmid <bjunior@seznam.cz>
2016-01-03 13:48:20 +0100

    Quality Value type changed to Double

--- OpenML/Response/Datasets/Quality.cs
+++ OpenML/Response/Datasets/Quality.cs
@@ -1,8 +1,12 @@
 ﻿namespace OpenML.Response.Datasets
 {
+    /// <summary>
+    /// OpenMl data quality with value
+    /// Describes information about the dataset as a whole, e.g. Number of features, number of instances etc.
+    /// </summary>
     public class Quality
     {
         public string Name { get; set; }
-        public string Value { get; set; }
+        public double Value { get; set; }
     }
 }
