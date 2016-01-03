d188448a41cffb159a99e7cb0c43a089fce6c8c7
jaksmid <jakub.smid@ktiml.mff.cuni.cz>
2016-01-03 11:23:40 +0100

    Added dataset file

---
+++ OpenML/Response/Tasks/Templates/Dataset.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenML.Response.Tasks.Templates
+{
+    public class Dataset
+    {
+        public string DataSetId { get; set; }
+        public string TargetFeature { get; set; }
+    }
+}
