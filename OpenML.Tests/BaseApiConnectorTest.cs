e3e3649451b69ebfea661e7e25c63b649059c5c7
jaksmid <jakub.smid@ktiml.mff.cuni.cz>
2016-03-17 17:49:11 +0100

    Added read only test key

--- OpenML.Tests/BaseApiConnectorTest.cs
+++ OpenML.Tests/BaseApiConnectorTest.cs
@@ -1,10 +1,14 @@
-﻿namespace OpenML.Tests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenML.Tests
 {
+    [TestClass]
     public class BaseApiConnectorTest
     {
         public BaseApiConnectorTest()
         {
-            Connector = new OpenMlConnector("");
+            //ReadOnlyKey for dotnet test account
+            Connector = new OpenMlConnector("57e587337295cc384add1de665563e29");
         }
 
         public OpenMlConnector Connector { get; set; }
